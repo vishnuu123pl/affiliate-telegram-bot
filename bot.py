8ee3b5beae1ac19dec1270e994788dfc9fe89a3e
VISHNUUUUUUUU <137207623+vishnuu123pl@users.noreply.github.com>
2026-05-27 19:26:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,37 @@
 import os
 from telegram import Update
-from telegram.ext import *
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-AMAZON_TAG = os.getenv("AMAZON_TAG")
+BOT_TOKEN = "5250059606:AAHmu7ZfuXzOmvjS6SHoNcx8nER4-d4xUkY"
+
+AMAZON_TAG = "fastdeals0ee-21"
+
+async def convert(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    text = update.message.text.strip()
+
+    if "amazon" in text or "amzn.in" in text:
+
+        if "?" in text:
+            link = text + f"&tag={AMAZON_TAG}"
+        else:
+            link = text + f"?tag={AMAZON_TAG}"
+
+        await update.message.reply_text(
+            f"✅ Amazon Affiliate Link Created\n\n{link}"
+        )
+
+    else:
+        await update.message.reply_text(
+            "❌ Send an Amazon link."
+        )
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+app.add_handler(
+    MessageHandler(filters.TEXT, convert)
+)
+
+print("Bot Running...")
+
+app.run_polling()
